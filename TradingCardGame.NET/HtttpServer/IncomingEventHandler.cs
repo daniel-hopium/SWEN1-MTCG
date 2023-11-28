adeb79e5c941421869c97cc931f1b37f6728c5c3
daniel <106086232+daniel-hopium@users.noreply.github.com>
2023-11-28 16:51:31 +0100

    Setup Http Server

---
+++ TradingCardGame.NET/HtttpServer/IncomingEventHandler.cs
@@ -0,0 +1,7 @@
+﻿namespace TradingCardGame.NET
+{
+    /// <summary>Implements an event handler for Incoming HTTP events.</summary>
+    /// <param name="sender">Sender.</param>
+    /// <param name="e">Event arguments.</param>
+    public delegate void IncomingEventHandler(object sender, HttpSvrEventArgs e);
+}
